09c10fff35335a300af4e73e50ba891b8e21d339
jorge hernandez <jorgehernandez@jorges-MacBook-Pro.local>
2021-11-25 18:18:18 -0500

    final changes

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,10 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives.{complete, extractDataBytes, onComplete, path, post}
+import akka.http.scaladsl.server.Directives.{complete, extractDataBytes, onComplete, path, post, withoutSizeLimit}
 import akka.pattern.after
-import akka.stream.scaladsl.{Flow}
+import akka.stream.scaladsl.Flow
 import akka.util.ByteString
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
@@ -11,23 +12,27 @@ import scala.concurrent.duration.DurationInt
 
 object main extends App {
 
-  implicit val system: ActorSystem = ActorSystem("BackPressureExample")
+  val config = ConfigFactory.load()
+  implicit val system: ActorSystem = ActorSystem("BackPressureExample", config)
   implicit val executionContext = system.dispatcher
   val flow = Flow[ByteString].log("error logging").mapAsync(1)(data =>{
     println("executing long operation")
-   after(2.seconds, system.scheduler)(Future.successful(data))
+   after(3.seconds, system.scheduler)(Future.successful(data))
   })
 
-  val route =
-    (post & path("test")) {
+  val route  =
+    withoutSizeLimit {
+      (post & path("test")) {
         extractDataBytes { bytes =>
           println("uploading")
           val stream = bytes.via(flow).run()
           onComplete(stream) { ioResult =>
             complete("Finished what ever I was doing: " + ioResult)
           }
+        }
       }
     }
 
-  val bindingFuture  =  Http().newServerAt("192.168.1.175", 8080).bind(route)
+
+  val bindingFuture  =  Http().newServerAt("localhost", 8080).bind(route)
 }
